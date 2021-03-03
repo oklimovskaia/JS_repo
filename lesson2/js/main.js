ec634ca8ee43f137b5c637fea901f021c404442e
oklimovskaia <olga.klimovskaja@gmail.com>
2021-03-02 20:59:19 -0500

    HW3

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -1,3 +1,27 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+// Перевести на Promise НЕ ИСПОЛЬЗОВАТЬ fetch
+
+
+const getRequest = (url) => {
+  return new Promise ((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status !== 200) {
+          reject(xhr.status);
+        } else {
+          resolve(xhr.responseText);
+        }
+      }
+    }  
+  });
+}
+
+
+
+///////////////////////////////////////
+
 class ProductList {
   #goods;
   #allProducts;
@@ -5,34 +29,48 @@ class ProductList {
   constructor(container = '.products') {
     console.log('constructor');
     this.container = container;
-    // this._goods = [];
     this.#goods = [];
     this.#allProducts = [];
 
-    this.#fetchGoods();
-    this.#render();
+    // this.#fetchGoods();
+    // this.#render();
+    this.#getProducts()
+        .then((data) => {
+          this.#goods = [...data];
+          this.#render();
+        });
   }
 
-  goodsTotalPrice() { 
-  let result = 0;
-    this.#goods.forEach((good) => {
-    
-     result += good.price;
-    });
-  return result;
-  }
-  
-  getTotalWithDiscount(discount) {
-    return this.goodsTotalPrice() * discount;
+  goodsTotalPrice() {
+    return this.#goods.reduce((sum, { price }) => sum + price, 0);
   }
 
-  #fetchGoods() {
-    this.#goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000},
-      {id: 4, title: 'Gamepad', price: 4500},
-    ];
+  // #fetchGoods() {
+  //   getRequest(`${API}/catalogData.json`, (data) => {
+  //     // console.log(data);
+  //     this.#goods = JSON.parse(data);
+  //     this.#render();
+  //     console.log(this.#goods);
+  //     console.log(this.#allProducts);
+  //   });
+  // }
+#fetchGoods() {
+     getRequest(`${API}/catalogData.json`).then((data) => {
+  //     // console.log(data);
+     this.#goods = JSON.parse(data);
+      this.#render();
+      console.log(this.#goods);
+      console.log(this.#allProducts);
+     }).catch((err) => {
+       console.log(err)
+     });
+    }
+  #getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then((response) => response.json())
+        .catch((err) => {
+          console.log(err);
+        });
   }
 
   #render() {
@@ -49,7 +87,7 @@ class ProductList {
 
 class ProductItem {
   constructor(product, img='https://placehold.it/200x150') {
-    this.title = product.title;
+    this.title = product.product_name;
     this.price = product.price;
     this.id = product.id;
     this.img = img;
@@ -73,6 +111,35 @@ class Cart {
   #cartItems;
   goodsTotalPrice() {}
   getTotalWithDiscount() {}
+
+  constructor() {
+    this.#cartItems = [];
+  }
+
+  addToCart(cartItem) {
+    for (i = 0; i < this.#cartItems.length; i++) {
+      if (this.#cartItems[i].getGoodId() == cartItem.getGoodId()) {
+        this.#cartItems[i].addQuantityOfGoods(1);
+        return;
+      }
+    };
+    this.#cartItems.push(cartItem);
+  }
+
+  removeFromCart(cartItem) {
+    let removedItemIndex = 0;
+    for (i = 0; i < this.#cartItems.length; i++) {
+      if (this.#cartItems[i].getGoodId() == cartItem.getGoodId()) {
+        break;
+      }
+      removedItemIndex++;
+    };
+    this.#cartItems.splice(removedItemIndex, 1);
+  }
+
+  getCartItems() {
+    return this.#cartItems;
+  }
 }
 
 class CartItem {
@@ -88,25 +155,11 @@ class CartItem {
     return this.quantityOfGoods * this.countPriceWithDiscount();
   }
 
+  getGoodId() {
+    return this.#good.id;
+  }
+
+  addQuantityOfGoods(num) {
+    this.#quantityOfGoods += num;
+  }
 }
-// const products = [
-//   {id: 1, title: 'Notebook', price: 20000},
-//   {id: 2, title: 'Mouse', price: 1500},
-//   {id: 3, title: 'Keyboard', price: 5000},
-//   {id: 4, title: 'Gamepad', price: 4500},
-// ];
-//
-// const renderProduct = ({ title, price }, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
-//               <img src="${img}" alt="Some img">
-//               <div class="desc">
-//                   <h3>${title}</h3>
-//                   <p>${price} \u20bd</p>
-//                   <button class="buy-btn">Купить</button>
-//               </div>
-//           </div>`;
-//
-// const renderProducts = list => {
-// document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
-// };
-//
-// renderProducts(products);
