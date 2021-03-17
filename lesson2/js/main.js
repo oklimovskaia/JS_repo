64dc72faf6b0f1d5cbccce355f3c148cce6e9bdd
oklimovskaia <olga.klimovskaja@gmail.com>
2021-03-16 22:28:36 -0400

    HomeWork6

--- lesson2/js/main.js
+++ lesson2/js/main.js
@@ -21,43 +21,6 @@ const app = new Vue({
                 .catch(error => {
                     console.log(error);
                 })
-        },
-        addProduct(product) {
-            this.getJson(`${API}/addToBasket.json`)
-                .then(data => {
-                    if (data.result < 1) {
-                        return;
-                    }
-                    for (i = 0; i < this.cartItems.length; i++) {
-                        if (this.cartItems[i].id_product == product.id_product) {
-                            this.cartItems[i].quantity++;
-                            return;
-                        }
-                    }
-                    this.cartItems.push(Object.assign({quantity:1}, product));
-                })
-        },
-
-        remove(item) {
-            this.getJson(`${API}/deleteFromBasket.json`)
-              .then(data => {
-                if (data.result < 1) {
-                    return;
-                }
-                if (item.quantity > 1) {
-                    item.quantity--;
-                } else {
-                    this.cartItems.splice(this.cartItems.indexOf(item), 1)
-                }
-                
-              })
-          },
-
-
-        filterGoods() {
-            let regexp = new RegExp(this.searchLine, 'i');
-            this.filtered = this.products.filter(el => regexp.test(el.product_name));
-            
         }
     },
     beforeCreate() {
@@ -71,21 +34,7 @@ const app = new Vue({
         console.log('beforeMount');
     },
     mounted() {
-        this.getJson(`${API + this.cartUrl}`)
-      .then(data => {
-        for (let el of data.contents) {
-          this.cartItems.push(el);
-        }
-        this.cartItems = [];
-      });
-      
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for (let el of data) {
-          this.products.push(el);
-          this.filtered.push(el);
-        }
-      });
+        console.log('mounted ' + this);
     },
     beforeUpdate() {
         console.log('beforeUpdate');
