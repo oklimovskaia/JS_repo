ce92beb946b94df3a2e6712f2f13dceb57492a5a
oklimovskaia <olga.klimovskaja@gmail.com>
2021-03-20 21:03:08 -0400

    HW7

---
+++ project_express_single/public/js/main.js
@@ -0,0 +1,79 @@
+// /const API = 'http//localhost:8082';//'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        searchLine: '',
+    
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        postJson(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(result => result.json())
+              .catch(error => {
+                  this.$refs.error.setError(error);
+              });
+        },
+        putJson(url, data) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }).then(result => result.json())
+              .catch(error => {
+                  this.$refs.error.setError(error);
+              });
+        },
+        deleteJson(url) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            }).then(result => result.json())
+              .catch(error => {
+                  this.$refs.error.setError(error);
+              });
+        },
+    },
+    beforeCreate() {
+        console.log('beforeCreate');
+    },
+    created() {
+        console.log('created');
+        
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted ' + this);
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate');
+    },
+    updated() {
+        console.log('updated');
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy');
+    },
+    destroyed() {
+        console.log('beforeDestroy');
+    }
+});
